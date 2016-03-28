7ebbd302081f544ffe1890bfdd44d7b28f996507
rumatoest <rumatoest@users.noreply.github.com>
2016-03-28 17:16:45 +0300

    Some changes

--- config/sublimity.el
+++ config/sublimity.el
@@ -2,11 +2,14 @@
 
 (ensure-packages-installed '(sublimity))
 
+;; (setq sublimity-scroll-weight 5 sublimity-scroll-drift-length 10)
+
 (require 'sublimity)
-(require 'sublimity-scroll)
 (require 'sublimity-map)
+(require 'sublimity-scroll)
 ;; (require 'sublimity-attractive)
 
 ;; If you want to enable sublimity everywhere, call function sublimity-global-mode.
 ;; (sublimity-global-mode)
+
 
