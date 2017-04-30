bf5483d83d610ab5e03ae15164440a1265d62a65
rumatoest <rumatoest@users.noreply.github.com>
2017-04-30 21:25:49 +0300

    Fix bulue in term

--- init.el
+++ init.el
@@ -58,8 +58,12 @@
    ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
  '(custom-enabled-themes (quote (deeper-blue)))
  '(line-move-visual t)
- '(rust-rustfmt-bin "~/.cargo/bin/rustfmt"))
+ '(rust-rustfmt-bin "~/.cargo/bin/rustfmt")
+ '(speedbar-show-unknown-files t)
+ '(tool-bar-mode nil))
+
 (custom-set-faces
+ (if (not window-system) '(default ((t (:background "nil"))))) ; Fix ugly blue color in terminal mode
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
